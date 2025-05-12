da5eaedd415d4ea56c2f1fc0d76958e599cff1a4
Hovooo <avakiandev@gmail.com>
2025-05-12 17:45:42 +0300

    Added tests for jUSD

--- test/JigsawUSD/JigsawUSD.t.sol
+++ test/JigsawUSD/JigsawUSD.t.sol
@@ -8,9 +8,9 @@ import { IERC20, IERC20Metadata } from "@openzeppelin/contracts/token/ERC20/exte
 
 import { Math } from "@openzeppelin/contracts/utils/math/Math.sol";
 
-import { BasicContractsFixture } from "../fixtures/BasicContractsFixture.t.sol";
+import { BasicContractsFixture, Manager } from "../fixtures/BasicContractsFixture.t.sol";
 
-import { JigsawUSD } from "../../src/JigsawUSD.sol";
+import { IJigsawUSD, JigsawUSD } from "../../src/JigsawUSD.sol";
 
 contract JigsawUsdTest is BasicContractsFixture {
     using Math for uint256;
@@ -75,5 +75,44 @@ contract JigsawUsdTest is BasicContractsFixture {
 
         vm.expectRevert(bytes("1000"));
         jUsd.burnFrom(user, 100);
+    }
+
+    function test_updateManager() public {
+        address newManager = address(new Manager(OWNER, address(weth), address(jUsdOracle), bytes("")));
+
+        uint256 updateTimestamp = block.timestamp;
+        vm.expectEmit();
+        emit IJigsawUSD.NewManagerRequested(newManager);
+        vm.prank(OWNER);
+        jUsd.requestNewManager(newManager);
+
+        vm.assertEq(address(jUsd.manager()), address(manager), "Old manager changed wrongfully");
+        vm.assertEq(jUsd.newManager(), newManager, "New manager wasn't saved");
+        vm.assertEq(jUsd.newManagerTimestamp(), updateTimestamp, "newManagerTimestamp wrong");
+
+        vm.expectRevert(bytes("3066"));
+        vm.prank(OWNER);
+        jUsd.acceptManager();
+
+        skip(2 hours);
+        vm.expectEmit();
+        emit IJigsawUSD.ManagerUpdated(address(manager), newManager);
+        vm.prank(OWNER);
+        jUsd.acceptManager();
+
+        vm.assertEq(address(jUsd.manager()), newManager, "Updated manager is wrong");
+        vm.assertEq(address(jUsd.newManager()), address(0), "New manager is not reset to address(0)");
+        vm.assertEq(jUsd.newManagerTimestamp(), 0, "newManagerTimestamp is not reset to 0");
+
+        address newStablesManager = vm.randomAddress();
+        uint256 testMintAmt = 420;
+
+        vm.prank(OWNER);
+        Manager(newManager).setStablecoinManager(newStablesManager);
+
+        vm.prank(newStablesManager);
+        jUsd.mint(address(this), testMintAmt);
+
+        vm.assertEq(jUsd.balanceOf(address(this)), testMintAmt, "Tokens not minted");
     }
 }
