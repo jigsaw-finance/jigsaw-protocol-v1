38009de671f1ab0ad178817ec22dd41598e9ff92
Hovooo <avakiandev@gmail.com>
2025-05-12 17:22:38 +0300

    Updated JigsawUSD interface

--- src/interfaces/core/IJigsawUSD.sol
+++ src/interfaces/core/IJigsawUSD.sol
@@ -10,10 +10,27 @@ import { IManager } from "./IManager.sol";
  * @dev Interface for the Jigsaw Stablecoin Contract.
  */
 interface IJigsawUSD is IERC20 {
+    // -- Events --
+
     /**
      * @notice event emitted when the mint limit is updated
      */
     event MintLimitUpdated(uint256 oldLimit, uint256 newLimit);
+
+    /**
+     * @notice Event emitted when a new manager is requested
+     * @param newManager The address of the requested new manager
+     */
+    event NewManagerRequested(address newManager);
+
+    /**
+     * @notice Event emitted when the manager is updated
+     * @param oldManager The address of the old manager
+     * @param newManager The address of the new manager
+     */
+    event ManagerUpdated(address oldManager, address newManager);
+
+    // -- State Variables --
 
     /**
      * @notice Contract that contains all the necessary configs of the protocol.
@@ -25,6 +42,8 @@ interface IJigsawUSD is IERC20 {
      * @notice Returns the max mint limit.
      */
     function mintLimit() external view returns (uint256);
+
+    // -- Owner specific methods --
 
     /**
      * @notice Sets the maximum mintable amount.
@@ -42,6 +61,46 @@ interface IJigsawUSD is IERC20 {
     function updateMintLimit(
         uint256 _limit
     ) external;
+
+    /**
+     * @notice Registers manager change request.
+     *
+     * @notice Requirements:
+     * - Contract must not be in active change.
+     * - New manager must be different from current manager.
+     * - New manager must not be the zero address.
+     *
+     * @notice Effects:
+     * - Updates the `newManager` state variable.
+     * - Updates the `newManagerTimestamp` state variable.
+     *
+     * @notice Emits:
+     * - `NewManagerRequested` event indicating successful manager change request.
+     *
+     * @param _manager The address of the new Manager contract.
+     */
+    function requestNewManager(
+        address _manager
+    ) external;
+
+    /**
+     * @notice Updates the manager.
+     *
+     * @notice Requirements:
+     * - Contract must be in active change.
+     * - Timelock must expire.
+     *
+     * @notice Effects:
+     * - Updates the `manager` state variable.
+     * - Resets the `newManager` state variable.
+     * - Resets the `newManagerTimestamp` state variable.
+     *
+     * @notice Emits:
+     * - `ManagerUpdated` event indicating successful manager change.
+     */
+    function acceptManager() external;
+
+    // -- Write type methods --
 
     /**
      * @notice Mints tokens.
